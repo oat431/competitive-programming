3b72a9238a946914263029de73b26bd7666632d3
oat431 <oat431@gmail.com>
2023-12-03 04:32:51 +0700

    🔨 update: solution for problem 0033

--- programming-in-th/Prb0033.java
+++ programming-in-th/Prb0033.java
@@ -1,30 +1,35 @@
-import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Prb0033 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int[] number = new int[n];
-		int[] like = new int[10000];
-		
-		for(int i=0; i<number.length; i++) {
-			number[i] = in.nextInt();
-			like[number[i]]++;
-		}
-        in.close();
-		
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
 
-		int max=0;
-		for(int i=0; i<number.length; i++) {
-			if(max<=like[number[i]]) {
-				max = like[number[i]];
-			}
-		}
-		for(int i=0; i<like.length; i++) {
-			if(like[i] == max) {
-				System.out.print(i+" ");
+        Map<Integer, Integer> map = new HashMap<>();
+        String[] numbers = in.readLine().split(" ");
+
+        for(int i=0; i<n; i++) {
+            int num = Integer.parseInt(numbers[i]);
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num)+1);
+            } else {
+                map.put(num, 1);
             }
-		}
+        }
+        in.close();
+
+        final int MAX_LIKE = map.values().stream().max(Integer::compare).get();
+        map.keySet()
+            .stream()
+            .filter(i -> map.get(i) == MAX_LIKE).collect(Collectors.toList())
+            .stream().sorted().forEach(
+                i -> System.out.print(i + " ")
+            );
     }
 }
 
