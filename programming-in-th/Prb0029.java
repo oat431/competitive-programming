d0aec821980cd7156246305a2f49dfa67314c411
oat431 <oat431@gmail.com>
2023-12-03 03:15:16 +0700

    🔨 add: problem 0030

--- programming-in-th/Prb0029.java
+++ programming-in-th/Prb0029.java
@@ -3,24 +3,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Prb0029 {
-    public static void main(String[] args) {
-        String[] input = new String[2];
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            input = in.readLine().split(" ");
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        String[] input = in.readLine().split(" ");;
         System.out.println(frog(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
     }
-
-    private static int frog(int x, int y) {
-        if(y >= x) {
-            return y%x == 0 ? (y/x) : (y/x) + 1;
-        } 
-        return 2;
-    }
+    private static int frog(int x, int y) { if(y >= x) { return y%x == 0 ? (y/x) : (y/x) + 1; } return 2; }
 }
 
 // from : https://programming.in.th/tasks/0029
---
+++ programming-in-th/Prb0030.java
@@ -0,0 +1,26 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Prb0030 {
+    public static void main(String[] args) {
+        String number = new String();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            number = in.readLine();
+            in.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        int n;
+        int three = 0;
+        int eleven = 0;
+        for (char digit : number.toCharArray()) {
+            n = digit - '0';
+            three = (three * 10 + n) % 3;
+            eleven = (eleven * 10 + n) % 11;
+        }
+        System.out.println(three+ " " +eleven);
+    }
+}
