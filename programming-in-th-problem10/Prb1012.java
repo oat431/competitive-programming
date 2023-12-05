46687ffd737d74de06598d21095549c91c0ee2df
oat431 <oat431@gmail.com>
2023-12-05 23:34:07 +0700

    🔨 add: problem 1012

---
+++ programming-in-th-problem10/Prb1012.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Prb1012 {
+    private static final Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = input.nextInt();
+        double sum = 0;
+        for(int i=0;i<n;i++) {
+            sum += input.nextInt() * 1.0;
+            sum += input.nextInt() * 0.75;
+            sum += input.nextInt() * 0.5;
+            sum += input.nextInt() * 0.25;
+            sum += input.nextInt() * 0.125;
+        }
+        System.out.println((int)Math.ceil(sum));
+    }
+}
