fb7e765e1f84a66454fea669cb1a008e49bc579a
oat431 <oat431@gmail.com>
2023-12-10 03:06:01 +0700

    🔨 add: codecube 004 solution

--- codecube-problem00/CodeCube004.java
+++ codecube-problem00/CodeCube004.java
@@ -2,7 +2,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class CodeCube0004 {
+public class CodeCube004 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
