8b615ea8277b6288f5ad65da4aaa84159e9e48f9
oat431 <oat431@gmail.com>
2023-12-10 02:28:45 +0700

    🔨 add : problem codecube 002

---
+++ codecube-problem00/CodeCube002.java
@@ -0,0 +1,26 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class CodeCube002 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            a = Integer.parseInt(br.readLine());
+            b = Integer.parseInt(br.readLine());
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(gcd(a, b));
+
+    }
+
+    private static int gcd(int a, int b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
+}
