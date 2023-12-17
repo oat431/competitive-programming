a4eea07de61888adae849cb813a6e2546f030a10
oat431 <oat431@gmail.com>
2023-12-17 18:45:40 +0700

    🔨 add: problem 1000

---
+++ programming-in-th-problem100/Prb1000.java
@@ -0,0 +1,44 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Prb1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        // word length
+        int wl = Integer.parseInt(in.readLine());
+        int n = Integer.parseInt(in.readLine());
+        String[] words = new String[n];
+        for(int i=0;i<n;i++) {
+            words[i] = in.readLine();
+        }
+        in.close();
+
+        System.out.println(wordChain(words, wl));
+    }
+
+    private static String wordChain(String[] words, int wl) {
+        String chkWord = words[0];
+        for(String word : words) {
+            if(word.equals(chkWord)) {
+                continue;
+            }
+            int diff = 0;
+            for(int i=0;i<wl;i++) {
+                if (chkWord.charAt(i) != word.charAt(i)) {
+                    diff++;
+                }
+                if(diff > 2) {
+                    break;
+                }
+            }
+            if(diff <= 2) {
+                chkWord = word;
+            } else {
+                break;
+            }
+        }
+        return chkWord;
+    }
+}
